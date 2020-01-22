4276f9a3fe2a7f43939bc12c7b66541ee6d86b28
ShocKwav3_ <ahmonemon@gmail.com>
2020-01-22 21:53:38 +0200

    added typescript with one sample tsx file

--- src/components/activitiesView/ActivitiesView.tsx
+++ src/components/activitiesView/ActivitiesView.tsx
@@ -8,16 +8,22 @@ import {
   getTimeString,
 } from '../../helpers/utils'
 import StatusesContainer from '../../containers/StatusesContainer'
+import React, { FunctionComponent } from 'react'
 
 
-export default function ActivitiesView({sensorData}) {
+type ActivitiesViewProps = {
+  sensorData: Array<Object>,
+}
+
+
+const ActivitiesView: FunctionComponent<ActivitiesViewProps> = ({sensorData}) => {
   const classes = activitiesViewStyles()
 
   return <Grid container className={classes.container} style={{backgroundColor: 'grey'}} spacing={2}>
     {
-      sensorData.map((value, index) => (
+      sensorData.map((value: any, index: any) => (
         <Grid key={index} item xs={12} sm={12} md={6} lg={2} xl={2}>
-          <CardItem containerStyle={classes.cardContainer}
+          <CardItem containerStyle={classes.container}
                     title={getTimeString(value.created_timestamp)}
                     footer={getDateString(value.created_timestamp)}
                     value={value}>
@@ -28,3 +34,5 @@ export default function ActivitiesView({sensorData}) {
     }
   </Grid>
 }
+
+export default ActivitiesView
